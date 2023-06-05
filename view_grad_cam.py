346679a598297f62f90f566552460841a820d079
snail <71243945+1-snail@users.noreply.github.com>
2023-06-05 15:59:06 +0800

    Update view_grad_cam.py

--- view_grad_cam.py
+++ view_grad_cam.py
@@ -1,16 +1,16 @@
 import argparse
-from datetime import date
 
 import torch
 from PIL import Image
 from torchvision import transforms
-from torchvision.models import vgg19, resnet18,convnext_base
-
+from torchvision.models import vgg19, resnet50,convnext_base
+import torchvision
 from grad_cam_resnet_feature import GradCamFeature ,draw_hotmap
 
-import cv2
-import  numpy as np
-import matplotlib.pyplot  as plt
+
+import os
+os.environ["KMP_DUPLICATE_LIB_OK"]="True"
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Test Grad-CAM')
     parser.add_argument('--image_name', default='both.png', type=str, help='the tested image name')
@@ -21,14 +21,11 @@ if __name__ == '__main__':
     IMAGE_NAME = opt.image_name
     SAVE_NAME = opt.save_name
     test_image = (transforms.ToTensor()(Image.open(IMAGE_NAME))).unsqueeze(dim=0)
-    model = convnext_base()
-    #model = resnet18(pretrained=True)
-    # model = vgg19(pretrained=True)
+    model = resnet50(weights=torchvision.models.ResNet50_Weights.IMAGENET1K_V1)
     if torch.cuda.is_available():
         test_image = test_image.cuda()
         model.cuda()
     grad_cam = GradCamFeature(model)
-    feature_images = grad_cam(test_image)#.squeeze(dim=0)
-   #feature_images = feature_images.permute((0,2,3,1))
-    draw_hotmap(feature_images,2)
+    feature_images = grad_cam(test_image)
+    draw_hotmap(feature_images,"resnet50")
 
